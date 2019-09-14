02420bc3e605875f0fef982f4288b70d7e45dbb4
Barbara Whiting <whitingba@gmail.com>
2019-09-13 19:34:05 -0600

    update server.js apiRoutes.js, and friends.js

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,22 @@
+var newFriends = require("../data/friends")
+
+
+module.exports = function (app) {
+
+    // API GET Requests
+    app.get("/api/friends", function (req, res) {
+        res.json(tableData);
+    });
+
+
+
+    // API POST Requests
+
+    app.post("/api/friends", function (req, res) {
+
+        //code here to test compatibility with other people in the database
+
+    });
+
+
+};
--- server.js
+++ server.js
@@ -2,3 +2,25 @@
 
 var express = require("express");
 var path = require("path");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port.
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+// The below code effectively "starts" our server
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
