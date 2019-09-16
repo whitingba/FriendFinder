a32e9e5a6012bf52f7c26406c3e6dc68f6e43219
Barbara Whiting <whitingba@gmail.com>
2019-09-15 22:04:19 -0600

    update pseudocoding

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,11 +13,20 @@ module.exports = function (app) {
     // API POST Requests
 
     app.post("/api/friends", function (req, res) {
+        //console.log(req.body);
+        //pull in the data from the survey
+        let newUserScores = req.body.scores;
+        //create an empty array to store the compared scores
+        let compareScoresArray = [];
+        //create a variable for the new friend match
+        let bestFriend = 0;
 
-        //code here to test compatibility with other people in the database
 
+        //loop through the user data
+
+        //loop through the database answeres
         //compare the newUser's scores against the 'allFriends' database
-        // will need to have a for loop that loops through both adding the difference between the two scores to a variable and recording the total difference to a new array
+        //while storing/keeping track of the compared answers
 
         //compare the total difference in score between the newUser and allFriends and the closest in value will be their match
 
