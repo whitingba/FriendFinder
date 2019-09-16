c7299ea65521692325c04cd3e04f83a9f0e797d6
Barbara Whiting <whitingba@gmail.com>
2019-09-15 20:59:32 -0600

    pseduo code what is left to be done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,11 @@
-let newFriends = require("../data/friends");
+let allFriends = require("../data/friends");
 
 
 module.exports = function (app) {
 
     // API GET Requests
     app.get("/api/friends", function (req, res) {
-        res.json(newFriends);
+        res.json(allFriends);
     });
 
 
@@ -15,6 +15,11 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
 
         //code here to test compatibility with other people in the database
+
+        //compare the newUser's scores against the 'allFriends' database
+        // will need to have a for loop that loops through both adding the difference between the two scores to a variable and recording the total difference to a new array
+
+        //compare the total difference in score between the newUser and allFriends and the closest in value will be their match
 
 
     });
